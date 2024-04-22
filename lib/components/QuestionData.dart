66f9962b5d28893289e689a2813b1c61003f6115
unknown <khanakbar0233@gmail.com>
2024-04-22 12:31:02 +0500

    full complete quiz app using oop

--- lib/components/QuestionData.dart
+++ lib/components/QuestionData.dart
@@ -1,12 +1,45 @@
 import 'package:quiz_app/components/question.dart';
 
 class QuestionData{
-  List questionBank=[
-    Question(questionText: 'The sun is a planet.', answerText: false,),
-    Question(questionText: 'Nawas Sharif is a loyal person', answerText: false),
-    Question(questionText: 'Water boils at 100°C at sea level.', answerText: true),
-    Question(questionText: 'The Great Wall of China is visible from space with the naked eye.', answerText: false),
-    Question(questionText: 'Oxygen is the most abundant element in Earth\s atmosphere.', answerText: false),
-    Question(questionText: "pakistran is a very rich country", answerText: false),
+  int _questionNumber = 0;
+  List _questionBank=[
+    // Question(questionText: 'The sun is a planet.', answerText: false,),
+    // Question(questionText: 'Nawas Sharif is a loyal person', answerText: false),
+    // Question(questionText: 'Water boils at 100°C at sea level.', answerText: true),
+    // Question(questionText: 'The Great Wall of China is visible from space with the naked eye.', answerText: false),
+    // Question(questionText: 'Oxygen is the most abundant element in Earth\s atmosphere.', answerText: false),
+    // Question(questionText: "pakistran is a very rich country", answerText: false),
+    Question(questionText: 'ARP is used to resolve IP addresses to MAC addresses.', answerText: true,),
+    Question(questionText: 'OSPF is a distance-vector routing protocol.', answerText: false,),
+    Question(questionText: 'VLANs operate at Layer 3 of the OSI model.', answerText: false,),
+    Question(questionText: 'A /30 subnet mask provides 2 usable IP addresses.', answerText: true,),
+    Question(questionText: 'EIGRP is a Cisco proprietary routing protocol.', answerText: true,),
+    Question(questionText: 'TCP is a connectionless protocol.', answerText: false,),
+    Question(questionText: 'SSH operates on port 22.', answerText: true,),
+    Question(questionText: 'RSTP is an improvement over STP to provide faster convergence.', answerText: true,),
+    Question(questionText: 'The default administrative distance for OSPF is 90.', answerText: false,),
+    Question(questionText: 'ICMP is used for error reporting and diagnostics.', answerText: true,),
   ];
+  String getQuestionData(){
+   return _questionBank[_questionNumber].questionText;
+  }
+  bool getanswerText(){
+   return _questionBank[_questionNumber].answerText;
+  }
+  void nextQuestion(){
+    if(_questionNumber < _questionBank.length -1)
+    _questionNumber++;
+  }
+  bool isFinished(){
+    if(_questionNumber >=_questionBank.length-1){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  resetQuestionNumber(){
+    _questionNumber=0;
+  }
+
 }
--- lib/quizz.dart
+++ lib/quizz.dart
@@ -12,123 +12,151 @@ class QuizzApp extends StatefulWidget {
 }
 
 class _QuizzAppState extends State<QuizzApp> {
-  // List<bool> answers = [false, false, true, false, false, false];
+
   List<Icon> scoreKeeper = [];
   QuestionData questionData = QuestionData();
-  int questionNumber = 0;
+
+
+  showIcons(bool userPickedAnswer){
+    setState(() {
+      if(questionData.isFinished()==true){
+        showDialog(context: context, builder: (context){
+          return AlertDialog(
+            title: Text('Finished'),
+            content: Text('Bas kar Bhai our kitna kelega',style: TextStyle(fontSize: 18),),
+            actions: [
+              RawMaterialButton(
+                child: Text('SAE da ror'),
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          );
+        });
+        questionData.resetQuestionNumber();
+        scoreKeeper = [];
+
+      }
+     else {
+        bool correctAnswer = questionData.getanswerText(); //true
+        if (correctAnswer == userPickedAnswer) {
+          scoreKeeper.add(Icon(Icons.check_circle, color: Colors.green,));
+        } else {
+          scoreKeeper.add(Icon(Icons.close_rounded, color: Colors.red,
+          ));
+        }
+        questionData.nextQuestion();
+      }
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            //Question
-            Expanded(
-              flex: 5,
-              child: Center(
+      body: Stack(
+        children: [
+        // First background color
+        Container(
+        color: Colors.blue,
+        width: double.infinity,
+        height: double.infinity,
+      ),
+
+      // Second background color with opacity
+      Container(
+        color: Colors.red.withOpacity(0.5),
+        width: double.infinity,
+        height: double.infinity,
+      ),
+
+      // Third background color with opacity
+      Container(
+        color: Colors.green.withOpacity(0.3),
+        width: double.infinity,
+        height: double.infinity,
+      ),
+        SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              //Question
+              Expanded(
+                flex: 5,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      questionData.getQuestionData(),
+                      style: TextStyle(color: Colors.black, fontSize: 24),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+              // True
+              Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    questionData.questionBank[questionNumber].questionText,
-                    style: TextStyle(color: Colors.white, fontSize: 24),
-                    textAlign: TextAlign.center,
+                  child: RawMaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "True",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    fillColor: Colors.green,
+                    onPressed: () {
+
+                     showIcons(true);
+                    },
                   ),
                 ),
               ),
-            ),
-            // True
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: RawMaterialButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "True",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+              // False
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: RawMaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "False",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    fillColor: Colors.red,
+                    onPressed: () {
+                      showIcons(false);
+                    },
                   ),
-                  fillColor: Colors.green,
-                  onPressed: () {
-                    bool correctAnswer =  questionData.questionBank[questionNumber].answerText;
-                    if (correctAnswer == true) {
-                      scoreKeeper.add(Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ));
-                    } else {
-                      scoreKeeper.add(Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ));
-                    }
-                    setState(() {
-                      if (questionNumber <= 3) {
-                        questionNumber++;
-                      }
-                    });
-                  },
                 ),
               ),
-            ),
-            // False
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: RawMaterialButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    "False",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  fillColor: Colors.red,
-                  onPressed: () {
-                    bool correctAnswer = questionData.questionBank[questionNumber].answerText;
-                    if (correctAnswer == false) {
-                      scoreKeeper.add(Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ));
-                    } else {
-                      scoreKeeper.add(Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ));
-                    }
-                    setState(() {
-
-                        questionNumber++;
-
-                    });
-                  },
-                ),
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                children: scoreKeeper,
-                // children: [
-                //   const Icon(Icons.check, color: Colors.green,),
-                //   const Icon(Icons.close, color: Colors.red,),
-                //   const Icon(Icons.check, color: Colors.green,)
-                // ],
-              ),
-            ),
-            if (questionNumber > 5)
+        
               Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  'No more questions available.',
-                  style: TextStyle(color: Colors.white),
-                  textAlign: TextAlign.center,
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: scoreKeeper,
+                  // children: [
+                  //   const Icon(Icons.check, color: Colors.green,),
+                  //   const Icon(Icons.close, color: Colors.red,),
+                  //   const Icon(Icons.check, color: Colors.green,)
+                  // ],
                 ),
               ),
-          ],
+              // if (questionNumber >=5)
+              //   Padding(
+              //     padding: const EdgeInsets.all(16.0),
+              //     child: Text(
+              //       'No more questions available.',
+              //       style: TextStyle(color: Colors.white),
+              //       textAlign: TextAlign.center,
+              //     ),
+              //   ),
+            ],
+          ),
         ),
+      ],
       ),
     );
   }
